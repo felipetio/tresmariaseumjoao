0686ff48c3245483d9b0a1e1d4e33192d4862378
Felipe Vieira <felipe.tio@gmail.com>
2011-09-04 23:37:11 -0300

    centralizando janela

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,5 +1,12 @@
-//$(document).ready(function() {
-//  $('#intro').click(function() {
-//      $(this).fadeOut(2000)
-//    })
-//  });
+function center() {
+  content = $('#sitebox-wrap')
+  $('#sitebox-wrap').animate({
+    'top' : parseInt(Math.max($(document).scrollTop() - 20, $(document).scrollTop() + ($(window).height() - content.height() - 40) * 0.5)),
+    'left' : parseInt(Math.max($(document).scrollLeft() - 20, $(document).scrollLeft() + ($(window).width() - content.width() - 40) * 0.5))
+  }, 200);
+}
+
+$(document).ready(function() {
+  center()
+  $(window).resize(center);
+});
